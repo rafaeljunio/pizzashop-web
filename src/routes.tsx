bcb5b11b70fc6821d3dabaab39d4818702776718
Rafael Souza <rafael.souza@iexfy.com>
2024-02-25 09:54:21 -0300

    graphs

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,8 +1,10 @@
+/* eslint-disable simple-import-sort/imports */
 import { createBrowserRouter } from 'react-router-dom'
 
+import { NotFound } from './pages/404'
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
-import { Dashboard } from './pages/app/dashboard'
+import { Dashboard } from './pages/app/dashboard/dashboard'
 import { Orders } from './pages/app/orders/orders'
 import { SignIn } from './pages/auth/sign-in'
 import { SignUp } from './pages/auth/sign-up'
@@ -11,6 +13,7 @@ export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
+    errorElement: <NotFound />,
     children: [
       {
         path: '/',
